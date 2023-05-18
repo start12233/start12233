0a87e7a6a1b3dc4eaf3426da4d97d02d73cfc20f
xiaodang <2161693837@qq.com>
2023-05-18 09:06:27 +0800

    feat：修改

--- src/views/user-quick-login/index.vue
+++ src/views/user-quick-login/index.vue
@@ -1,115 +1,91 @@
 <script setup lang="ts">
 
 </script>
-
 <template>
-  <div class="user-quick-login">
-    <div class="picture">
+  <div class="container">
+    <div class="user-logo-information">
       <div class="logo">
         logo
       </div>
     </div>
     <div class="user-number">
-      12345
+      1234567
     </div>
     <div class="user-switch-account">
-      <a href="">切换账号</a>
+      <a>切换账号</a>
     </div>
-    <div class="login-btn">
+  </div>
+  <div class="user-login">
+    <div class="number-login">
       <div class="btn">
-        <a href="">本机号码一键登录</a>
+        手机号码一键登录
       </div>
     </div>
-    <div class="btn2">
-      <a href="">其他方式登录/注册</a>
+    <div class="switch-login-ways">
+      其他方式登录\注册
     </div>
-    <label class="user-checkbox">
-      <input type="checkbox">
-      <span>我已同意并阅读《天翼账号认证服务条款》和《隐私协议》</span>
-    </label>
+    <div class="bottom">
+      <input type="checkbox">我已阅读协议并同意《天翼账号认证服务条款》和《隐私协议》
+    </div>
   </div>
 </template>
-
 <style scoped lang="scss">
-*{
-  margin: 0rem;
-  font-size: 32rem;
-  font-family: PingFangSC;
+.container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  .user-logo-information{
+      margin-top: 80rem;
+      width: 128rem;
+      height: 128rem;
+      background-color: antiquewhite;
+    .logo{
+      line-height: 128rem;
+      width: 64rem;
+      height: 48rem;
+    }
+  }
+  .user-number{
+      font-size: 48rem;
+      margin-top: 80rem;
+      width: 272rem;
+      height: 64rem;
+      text-align: center;
+      line-height: 64rem;
+    }
+    .user-switch-account{
+      font-size: 32rem;
+      margin-top: 32rem;
+    }
 }
-.user-quick-login{
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: space-between;
-}
-.picture{
-  margin-top: 265rem;
-  width: 128rem;
-  height: 128rem;
-  justify-content: center;
-  position:relative;
-  background-color: antiquewhite;
-}
-.picture .logo{
-  margin: 40rem 32rem;
-  position: absolute;
-}
-.user-number{
-  margin-top: 80rem;
-  text-align: center;
-  align-items: center;
-}
-.user-switch-account{
-  margin-top: 32rem;
-  justify-content: center;
-}
-.user-switch-account a{
-  color:black;
-  text-decoration: none;
-  font-size: 32rem;
-}
+.user-login{
+    margin-top: 78rem;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+  .number-login{
+    width: 650rem;
+    height: 88rem;
+    background-color: rgba(50, 50, 50, 1);
+    border-radius: 50rem;
+    .btn{
+      margin-left: 25%;
+      height: 48rem;
+      line-height: 88rem;
+      font-size: 32rem;
+      color: white;
+    }
+  }
+  .switch-login-ways{
+    font-size: 32rem;
+    height: 40rem;
+    color:  rgba(0, 0, 0, 0.2);
+  }
+  .bottom{
+    flex:1 1 aoto;
+    .checkbox{
+    }
+  }
 
-.login-btn{
-  margin-top: 78rem;
-  align-content: center;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  position: relative;
-  width: 650rem;
-  height: 88rem;
-  border: none;
-  border-radius: 50rem;
-  background: rgba(50, 50, 50, 1);
-
-}
-
-.login-btn .btn{
-  display: flex;
-  flex-direction: column-reverse;
-  align-content: center;
-  width: 304rem;
-  height: 48rem;
-  position: absolute;
-}
-.login-btn .btn a{
-  color:white;
-  text-decoration: none;
-  font-size:32rem;
-}
- .btn2{
-  margin-top:40rem;
-  display: flex;
-  justify-content: center;
-}
- .btn2 a{
-  color:rgba(0, 0, 0, 0.2);
-  text-decoration: none;
-  font-size:32rem;
-}
-.user-checkbox{
-  margin-top: 622rem;
-  font-size: 22rem;
-  font-size: 22rem;
 }
 </style>
